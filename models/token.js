0900c1319a0276e74e19c8e49e7098a0416e4782
Brian Cabacungan <lbcab@uw.edu>
2021-07-30 02:17:16 -0700

    lbcab Week 4 Assignment Complete

--- models/token.js
+++ models/token.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-//make token unique per userId
+//token should not be unique
+//need to allow a user to have multiple tokens, which allows them to log in and out on multiple browsers/devices at same time
 const tokenSchema = new mongoose.Schema({
-  token: { type: String, required: true, unique: true },
+  token: { type: String, required: true },
   userId: { type: String, required: true }
 });
 
