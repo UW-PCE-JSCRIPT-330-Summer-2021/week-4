9413f2d0e53ffa028292139cdc1a54089056697f
pcpower <77902129+pcpower@users.noreply.github.com>
2021-08-20 13:27:36 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const routes = require("./routes");
 
-const middleware = require("./middleware");
+const middleware = require("./Middleware");
 
 const server = express();
 server.use(express.json());
